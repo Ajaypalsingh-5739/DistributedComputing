2a03e01ef8fba2b51eef682e3904f6c2cbc6f67b
Ajaypal91 <erajaypal91@gmail.com>
2017-09-25 21:40:31 -0700

    adding files for homework 3

---
+++ HM-3-DFS-With-Root/src/Buffer.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Observable;
+
+/**
+ * Created by tphadke on 8/29/17.
+ */
+public class Buffer extends Observable {
+    private Message message;
+
+    public Buffer(){
+        //Create an empty Buffer
+    	message = null;
+    }
+    public Buffer(Message message) {
+        this.message = message;
+    }
+
+    public Message  getMessage() {
+        return message;
+    }
+
+    /**
+     * 
+     * @param message
+     * @param arg is used to keep track of the message sending processor
+     */
+    public void setMessage(Message message , Processor arg ) {
+        this.message = message;
+        setChanged();
+        notifyObservers(arg);
+    }
+}
+
