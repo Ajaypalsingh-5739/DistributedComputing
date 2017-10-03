91083e6aa7322a608bf572ca73356a2c3243972e
Ajaypal91 <erajaypal91@gmail.com>
2017-10-02 18:27:04 -0700

    adding code

---
+++ HM-5-Calculate-VectoClocks/src/Algorithm.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class Algorithm {
+	int noOfProcessors;
+	Processor P; 
+	
+	/**
+	 * 
+	 * @param P
+	 */
+	public Algorithm(Processor P) {
+		this.noOfProcessors = 3;
+		this.P = P;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param computeMessage
+	 */
+	public void computeEvent() {
+		System.out.println("In Computation of Processor " + P.getID());
+		int newValue = P.getVectorClock().vc[P.getID()]+1;
+		P.vc.updateAt(P.getID(), newValue);
+		VectorClock newVC = new VectorClock(P.getVectorClock());
+		P.store.add(newVC);
+	}
+
+	/**
+	 * 
+	 * @param to
+	 */
+	public void sendEvent(Processor to) {
+		System.out.println("In Send Event of Processor " + P.getID());
+		computeEvent();
+		P.messageBuffer.addObserver(to);
+		P.messageBuffer.setMessage(new Message(MessageType.SEND, new VectorClock(P.getVectorClock())), P.getID());
+	}
+	
+
+}
