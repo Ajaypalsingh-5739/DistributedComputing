e36f57f09b3066114b4b2b3818f75be69dbb0df4
Ajaypal91 <erajaypal91@gmail.com>
2017-10-29 18:54:59 -0700

    changes from LEADER TO IDENTIFIER

--- HM-7-LeaderElection/src/MessageType.java
+++ HM-7-LeaderElection/src/MessageType.java
@@ -6,9 +6,7 @@ public enum  MessageType {
 	/**
 	 * Indicates that a computation event is being performed
 	 */
-    LEADER,
-    //TODO: Add more message types SEND and RECIEVE
-
-	TERMINATION
+    IDENTIFIER,
+    TERMINATION
     
 }
